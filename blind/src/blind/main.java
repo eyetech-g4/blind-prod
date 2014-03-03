935f2a02032f429cb2e1f45eaa7cce9b4d0f5029
Gnese <Renaud@PC-RENAUD>
2014-03-03 15:39:54 +0100

    commit new main

---
+++ blind/src/blind/main.java
@@ -0,0 +1,17 @@
+package blind;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
