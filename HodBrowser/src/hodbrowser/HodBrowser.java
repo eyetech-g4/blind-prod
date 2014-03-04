0ddf4375056b3ec3a08a94280beae19949417d87
nicolasmeucci <nicolas.meucci@free.fr>
2014-03-04 19:02:13 +0100

    Nicolas Meucci 04/03/2014 Ajout barre d'adresse

--- HodBrowser/src/hodbrowser/HodBrowser.java
+++ HodBrowser/src/hodbrowser/HodBrowser.java
@@ -1,12 +1,14 @@
 /*
-* To change this template, choose Tools | Templates
-* and open the template in the editor.
-*/
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package hodbrowser;
- 
+
 import java.awt.Dimension;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -15,57 +17,64 @@ import javafx.scene.web.WebView;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 
 public class HodBrowser extends Application {
-	
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-      launch(args);
-  }
- 
-  @Override
-  public void start(Stage primaryStage) {
-	  primaryStage.setTitle("HÖD");
-	  BorderPane affichage = new BorderPane();
-	  
-	  Screen screen = Screen.getPrimary();
-	  Rectangle2D bounds = screen.getVisualBounds();
+	BorderPane affichage = new BorderPane();
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-	  primaryStage.setScene(new Scene(affichage, 0, 0));
-	  
-	  primaryStage.setX(bounds.getMinX());
-	  primaryStage.setY(bounds.getMinY());
-	  primaryStage.setWidth(bounds.getWidth());
-	  primaryStage.setHeight(bounds.getHeight());
-	  
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("HÖD");
+		
 
-	  
-      affichage.setTop(getMenu());
-      affichage.setCenter(getPageWeb());
-      
+		Screen screen = Screen.getPrimary();
+		Rectangle2D bounds = screen.getVisualBounds();
 
-      primaryStage.show();
-  }
+		primaryStage.setScene(new Scene(affichage, 0, 0));
 
-  private ToolBar getMenu() {
-	  
-     ToolBar toolBar = new ToolBar();
-     Button button1 = new Button("Grosseur");
-     Button button2 = new Button("Contraste");
-     toolBar.getItems().addAll(button1, button2);
+		primaryStage.setX(bounds.getMinX());
+		primaryStage.setY(bounds.getMinY());
+		primaryStage.setWidth(bounds.getWidth());
+		primaryStage.setHeight(bounds.getHeight());
 
-     return toolBar;
-  }
-  
-  private WebView getPageWeb() {
-	  
-	WebView pageWeb = new WebView();
-	WebEngine myWebEngine = pageWeb.getEngine();
-	myWebEngine.load("https://www.google.fr/");
-	
-	return pageWeb;
-  }
+		affichage.setTop(getMenu());
+		affichage.setCenter(getPageWeb("https://www.google.fr/"));
+
+		primaryStage.show();
+	}
+
+	private ToolBar getMenu() {
+
+		ToolBar toolBar = new ToolBar();
+		Button buttonGo = new Button("Go");
+		final TextField addressBar = new TextField();
+		addressBar.setPrefWidth(800);
+		toolBar.getItems().addAll(buttonGo, addressBar);
+		buttonGo.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("mecouillessurtonfront");
+				affichage.setCenter(getPageWeb(addressBar.getText()));
+			}
+		});
+
+		return toolBar;
+	}
+
+	private WebView getPageWeb(String url) {
+
+		WebView pageWeb = new WebView();
+		WebEngine myWebEngine = pageWeb.getEngine();
+		myWebEngine.load(url);
+
+		return pageWeb;
+	}
 }
