7134808030c977508230dfc3da7c26c2c09900d8
Gnese <Renaud@PC-RENAUD>
2014-03-05 13:26:28 +0100

    test

--- HodBrowser/src/hodbrowser/test.java
+++ HodBrowser/src/hodbrowser/test.java
@@ -83,7 +83,7 @@ public class test extends Application {
 				try {
 					   
 				    synth = Central.createSynthesizer(new SynthesizerModeDesc(Locale.US));
-		            System.out.println(Locale.getDefault()+"ok");
+		            System.out.println(Locale.getDefault());
 		            synth.allocate();
 		            synth.resume();
 		            
