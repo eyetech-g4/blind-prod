65f13778dbf1613ca870b0609eb9fd745ad3f884
Gnese <Renaud@PC-RENAUD>
2014-03-05 11:02:35 +0100

    Renaud Deville 05/03 commit and push classe reconaissance vocale

---
+++ HodBrowser/src/hodbrowser/voiceRecognizer.java
@@ -0,0 +1,22 @@
+package hodbrowser;
+
+import java.util.Locale;
+
+import javax.speech.Central;
+import javax.speech.EngineList;
+import javax.speech.recognition.RecognizerModeDesc;
+
+public class voiceRecognizer {
+
+	public static Boolean isRecognitionEngine() {
+		RecognizerModeDesc desc = new RecognizerModeDesc(Locale.ENGLISH,
+				Boolean.TRUE);
+		EngineList el = Central.availableRecognizers(desc);
+		if (el.size() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
