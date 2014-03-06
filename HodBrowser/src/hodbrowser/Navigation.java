4c771fbac23f86cebded93070d2855fe60a8c3c1
Drake1609X4 <Drake1609X4@192.168.2.17>
2014-03-06 09:04:12 +0100

    arnaud sangay 06/03/14 restauration Navigation

--- HodBrowser/src/hodbrowser/Navigation.java
+++ HodBrowser/src/hodbrowser/Navigation.java
@@ -1,39 +1,47 @@
 package hodbrowser;
 
+import java.util.ArrayList;
+
 import javafx.scene.web.WebEngine;
 
 public class Navigation{
 	private int i=0;
-	private String[] URLpath=new String[10];
+	private ArrayList<String> URLpath=new ArrayList<String>();
 	private WebEngine usedWebEngine;
 	
 	public String getPreviousURLpath() {
-		System.out.println(i);
-		if((i>0)&&(i<10)){
+		//System.out.println(i);
+		if((URLpath.size()>0)&&(i>0)){
 			i--;
-			return URLpath[i];	
+			//System.out.println(i-1);
+			//System.out.println(URLpath.get(i));
+			return URLpath.get(i-1);	
 		}
 		else{
-			return URLpath[0];	
+			return URLpath.get(i);	
 		}
 		
 		
 	}
 	
 	public String getNextURLpath() {
-		System.out.println(i);
-		if((i>=0)&&(i<10)){
+		//System.out.println(i);
+		if((URLpath.size()>0)&&(i<URLpath.size())){
 			i++;
-			return URLpath[i];	
+			System.out.println(i);
+			System.out.println(URLpath.get(i-1));
+			return URLpath.get(i-1);	
 		}
 		else{
-			return URLpath[10];	
+			return URLpath.get(i-1);	
 		}
 	}
 	
 	protected void setURLpath() {
-		usedWebEngine = HodBrowser.getWebEngine();
-		this.URLpath[i]=usedWebEngine.getLocation().toString();		
+		usedWebEngine=HodBrowser.getWebEngine();
+		this.URLpath.add(usedWebEngine.getLocation().toString());
+		//System.out.println(URLpath.get(i));
+		System.out.println(i);
 		i++;
 	}
 	
